ea538792632e0a8097b4a1127326616d3f230ed4
Bryan Salva <salvatorresb@gmail.com>
2022-10-20 20:30:44 -0500

    proyecto base del sistema de atecion de citas

---
+++ src/main/java/bienvenidos/SistemaAtencionDeCitas.java
@@ -0,0 +1,12 @@
+
+package bienvenidos;
+
+import javax.swing.JOptionPane;
+
+
+public class SistemaAtencionDeCitas {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hola listos para comenzar?");
+    }
+}
