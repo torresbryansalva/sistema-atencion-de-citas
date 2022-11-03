ca5163f969432b17a0b72b63f2ac35efb74f4f2f
FransiSotz <flsotz@gmail.com>
2022-11-03 12:05:27 -0600

    Inicio_Sesion

--- src/main/java/bienvenidos/SistemaAtencionDeCitas.java
+++ src/main/java/bienvenidos/SistemaAtencionDeCitas.java
@@ -1,6 +1,7 @@
 
 package bienvenidos;
 
+import interfaces.Inicio_Sesion;
 import javax.swing.JOptionPane;
 
 
@@ -8,5 +9,7 @@ public class SistemaAtencionDeCitas {
 
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,"Hola listos para comenzar?");
+//        Inicio_Sesion i = new Inicio_Sesion();
+//        i.setVisible(true);
     }
 }
